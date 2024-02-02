26921dc1844b0ab6c82a538b332a9b2ce71b4f7f
SamuelPDS <duartes607@gmail.com>
2024-02-02 11:47:37 -0300

    feat: add style to the weather

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 
 const divTag = document.querySelector('.container__content')
 
-const url = 'https://api.weatherapi.com/v1/current.json?key=ea460dbe70f64fa1a70210336243101&q=Belo Horizonte'
+const url = 'https://api.weatherapi.com/v1/current.json?key=ea460dbe70f64fa1a70210336243101&q=Belo Horizonte&lang=pt'
 
 fetch(url).then((res) => res.json())
 .then((data) => {
-    const icon = data.current.condition.icon
-     const locationName = data.location.name;
-     const lastUptaded = data.current.last_updated;
-     const feelsLike = data.current.feelslike_c;
-     const temp = data.current.temp_c
-    // console.log(data) 
+    const icon = data.current.condition.icon;
+    const conditionText = data.current.condition.text;
+    const locationName = data.location.name;
+    const lastUptaded = data.current.last_updated;
+    const feelsLike = data.current.feelslike_c;
+    const temp = data.current.temp_c;
+        console.log(conditionText) 
       divTag.innerHTML = `
          <p class="container__paragraph">Atualizado em ${lastUptaded}</p>
          <p class="container__paragraph">Cidade: ${locationName}</p>
          <div class="container__weather">
-            <img src=${icon}>
-         <p class="container__paragraph-temp">${temp}°C</p>
+            <div class="container__paragraph-temp">
+                <img src=${icon}>
+                <p>${temp}°C</p>
+            </div>
+                <p class="container__paragraph-conditionText">${conditionText}</p>
          </div>
          <p class="container__paragraph-temp-feesLike">Sensação Térmica: ${feelsLike}</p>
          
